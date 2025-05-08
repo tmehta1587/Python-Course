c30c4629f584836df1511a3cebf5282663907faa
tmehta1587 <tmehta1587@gmail.com>
2025-05-08 18:09:30 +0700

    commit

---
+++ Module 1/activity 1.py
@@ -0,0 +1,8 @@
+
+print("Hello, World!")
+
+print("My name is Tinashe.")
+
+print("I love playing soccer.")
+
+print("The result of 3+5 is:",3+5)
---
+++ Module 1/activity 4.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the world of programming!")
+
+print(4)
+
+print("Hello Python \n")
+
+print("Keywords in Python are....")
+import keyword 
+print(keyword.kwlist)
