c50745d933a21f13555ce7d7ff39a8cd90d6977c
tmehta1587 <tmehta1587@gmail.com>
2025-05-15 18:12:52 +0700

    commit

---
+++ Module 3/activity 4.py
@@ -0,0 +1,10 @@
+
+import datetime 
+
+current_time = datetime.datetime.now()
+
+print("Time now at greenwich meridian is :", end = "")
+print(current_time)
+
+import calendar
+print("\n", calendar.calendar(2011))
