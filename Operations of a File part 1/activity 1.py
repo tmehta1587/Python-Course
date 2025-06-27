5b0da8d3d0a88168de5ceaa5bade3916a24349c4
tmehta1587 <tmehta1587@gmail.com>
2025-06-27 17:50:59 +0700

    commit

---
+++ Operations of a File part 1/activity 1.py
@@ -0,0 +1,13 @@
+
+file = open('Codingal.txt', 'r')
+print(file.read())
+file.close()
+
+file = open('Codingal.txt', 'r')
+print("\n Read in parts \n")
+print(file.read(8))
+file.close()
+
+file = open('Codingal.txt', 'a')
+file.write("Hi! I am Penguin and I am 1 year old.")
+file.close()
