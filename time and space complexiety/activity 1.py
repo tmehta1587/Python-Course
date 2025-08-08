f8b82ed140a85a0cf96ec8bd6858f0309095bd37
tmehta1587 <tmehta1587@gmail.com>
2025-08-08 18:24:12 +0700

    commit

---
+++ time and space complexiety/activity 1.py
@@ -0,0 +1,20 @@
+
+def fun1(n):
+    return n*(n+1)/2
+print(fun1(4))
+
+def fun2(n): 
+    sum=0
+    for i in range(1,n+1):
+        sum+=i
+    return sum
+print(fun2(4))
+
+def fun3(n):
+    sum=0
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            sum+=1
+    return sum
+print(fun3(4))
+
