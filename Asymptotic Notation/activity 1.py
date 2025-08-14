338e0380e4691d4711f0b7ab4d2a7b7c0d2c8140
tmehta1587 <tmehta1587@gmail.com>
2025-08-14 18:05:52 +0700

    commit

---
+++ Asymptotic Notation/activity 1.py
@@ -0,0 +1,10 @@
+
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is", n)
+    iteration+=1
+    print("Total iterations done by the code is",iteration,"\n")
+
+printnumber(10)
+printnumber(20)
+print("\n with any 'n' the time taken by our code won't change")
