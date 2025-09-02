5162d3fae9577a7b7edc4da765aab8d96a4ccead
tmehta1587 <tmehta1587@gmail.com>
2025-09-02 08:18:34 +0700

    commit

---
+++ Introduction to bit manipullation/act 3.py
@@ -0,0 +1,11 @@
+
+def numberOfBits(n):
+    count=0
+
+    while(n):
+        count += 1
+        n>>= 1
+    return count 
+
+number = int(input("Enter your number:"))
+print("Total bits:", numberOfBits(number))
