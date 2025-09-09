7df5fb983518a868cd2ae413ecb905aa000ab82a
tmehta1587 <tmehta1587@gmail.com>
2025-09-09 08:14:03 +0700

    commit

---
+++ Bitplay 1/activity 1.py
@@ -0,0 +1,17 @@
+
+def numberOfBits(n):
+    ones = 0
+    zeros = 0
+    
+    while(n):
+        if (n&1==1):
+            ones+=1
+        else:
+            zeros+=1
+
+        n>>=1
+
+    print("\n\n0nes =", ones,"nZeros", zeros)
+
+number = int(input("Enter your number:"))
+numberOfBits(number)
