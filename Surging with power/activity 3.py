b7e9bcb3f514ece89d1a46d20705093106e347e4
tmehta1587 <tmehta1587@gmail.com>
2025-09-18 08:11:40 +0700

    commit

---
+++ Surging with power/activity 3.py
@@ -0,0 +1,16 @@
+
+def computePower(x,y):
+    result = 1
+    print("Hi")
+    while(y>0):
+        if(y%2 ==0):
+            x = x*x
+            y>>=1
+        else: 
+            result = result * x
+            y=y-1
+    return result
+
+x = int(input("Enter x for x^y:"))
+y=int(input("Enter y for x^y:"))
+print("Total: ", (computePower(x,y)))
