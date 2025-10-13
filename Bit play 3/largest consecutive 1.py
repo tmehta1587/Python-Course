132ac010e6806e60b2c0962e3c29460afcf1b2ad
tmehta1587 <tmehta1587@gmail.com>
2025-10-13 11:48:20 +0700

    commit

--- Bit play 3/largest consecutive 1.py
+++ Bit play 3/largest consecutive 1.py
@@ -2,15 +2,15 @@
 def longest_consecutive_ones(n):
     count = 0
     max_count = 0
-    while n != 0:
-        if n & 1:
+    for bit in binary: 
+        if bit == '1':
             count += 1
             if count>max_count:
                 max_count = count
         else: 
             count = 0
-        n = n>>1
-        return max_count
+    return max_count
     
 num = int(input("Enter a number:"))
-print("Longest consecutive 1s:", longest_consecutive_ones(num))
+binary = bin(num)[2:]
+print("Longest consecutive 1s:", longest_consecutive_ones(binary) )
