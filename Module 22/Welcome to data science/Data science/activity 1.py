835d88a9c1b7ce6917b0c21d935a32901eed96be
tmehta1587 <tmehta1587@gmail.com>
2025-10-31 09:36:56 +0700

    commit

---
+++ Module 22/Welcome to data science/Data science/activity 1.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+a = np.array([3,3,6])
+print('First array:')
+print(a)
+print('\n')
+
+b = np.array([10, 10, 10])
+print('Second array: ')
+print(b)
+print('\n')
+
+print('Add the two arrays: ')
+print(np.add(a,b))
+print('\n')
+
+print('Subtract the two arrays:')
+print(np.subtract(a,b))
+print('\n')
+
+print(' Multiply the two arrays: ')
+print(np.divide(a,b))
+print('\n')
